5516c62b37e992b1b05c2424b38e72b90c1074f5
ahmatguerdi <ahmatguerdi9@gmail.com>
2025-09-24 11:25:49 +0100

    route, model, controller

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Conneté avec succès!");
+  })
+  .catch((error) => {
+    console.error("Erreur de connexion!:", error);
+  });
+
+  app.use(express.json());
+//   app.use('/api/auth', authRoutes);
+  app.listen(PORT, () => {
+    console.log(`Serveur est démarré sur le http://localhost: ${PORT}`);
+    
+  })
---
+++ models/usersModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      ttype: String,
+      minlength: 6,
+      required: true,
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Users", userSchema);
